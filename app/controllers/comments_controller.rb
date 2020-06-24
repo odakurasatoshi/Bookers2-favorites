70bc9a5bae844308801f3c87db7570fa84d83baf
vagrant <vagrant@localhost.localdomain>
2020-06-24 01:29:15 +0000

    [Finish]応用課題3

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,6 +6,11 @@ class CommentsController < ApplicationController
 		comment.save
 		redirect_to book_path(book)
 	end
+
+	def destroy
+		Comment.find_by(id: params[:id], book_id: params[:book_id]).destroy
+		redirect_to book_path(params[:book_id])
+	end
 	private
 	def comment_params
 		params.require(:comment).permit(:comment)
